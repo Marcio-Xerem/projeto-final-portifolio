20cfd91e4683655e4cc2631dbc56cecb9b39c7a0
Márcio da Mota Xerém <mxerem@icloud.com>
2024-10-25 23:06:45 -0300

    Preparando a pasta dist para rodar no GitHub Pages

--- src/routes/AvatarMarcioXerem.jsx
+++
@@ -1,8 +0,0 @@
-import fotoMarcioXerem from '../images/Marcio_Xerem.jpeg';
-
-export default function AvatarMarcioXerem() {    
-    return (
-        <div id="avatar">            
-            <img src={fotoMarcioXerem} alt="Foto de Márcio da Mota Xerém" />
-        </div> )       
-}
--- src/routes/ProjetoDecida.jsx
+++
@@ -1,8 +0,0 @@
-import imagemProgetoDecida from '../images/decida.jpeg';
-
-export default function ProjetoDecida() {    
-    return (
-        <div id="decida">            
-            <img src={imagemProgetoDecida} alt="Imagem do site do Projeto Decida" />
-        </div> )       
-}
